e9c4c85eff25a7b5e5b4420bf8a2fcda1f792bff
juanvalno <133802373+juanvalno@users.noreply.github.com>
2024-06-11 12:10:10 +0700

    Update streamlit.py

--- Streamlit/streamlit.py
+++ Streamlit/streamlit.py
@@ -24,10 +24,6 @@ if response_lambda.status_code == 200 and response_model.status_code == 200:
     optimal_lambdas = pickle.load(lambda_buffer)
     model = joblib.load(model_buffer)
 
-    # Debugging: Check the type and a few key attributes of the loaded model
-    st.write(f"Model type: {type(model)}")
-    st.write(f"Model parameters: {model.get_params()}")
-
     # Define all expected features
     expected_features = ['POV', 'FOOD', 'ELEC', 'WATER', 'LIFE', 'HEALTH', 'SCHOOL', 'STUNTING']
 
@@ -57,17 +53,10 @@ if response_lambda.status_code == 200 and response_model.status_code == 200:
     # Reorder the columns to match the expected feature order
     input_df = input_df[expected_features]
 
-    # Debugging: Check the transformed input data
-    st.write("Transformed input data:")
-    st.write(input_df)
-
     # Make predictions
     if st.button('Predict'):
-        try:
-            prediction = model.predict(input_df)
-            inverse_prediction = np.expm1(prediction)
-            st.write('Predicted IKP: {:.2f}'.format(inverse_prediction[0]))
-        except Exception as e:
-            st.error(f"Prediction error: {e}")
+        prediction = model.predict(input_df)
+        inverse_prediction = np.expm1(prediction)
+        st.write('Predicted IKP: {:.2f}'.format(inverse_prediction[0]))
 else:
     st.error("Failed to load model or transformation parameters. Please check the URLs.")
